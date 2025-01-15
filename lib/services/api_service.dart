a62a4185eea184024251362fe8c72d65497a4245
Iqbal F <iqbalfauzien@gmail.com>
2025-01-15 17:57:26 +0700

    fix: usermodel

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,130 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
+
+import '../models/user_model.dart';
 
 class ApiService {
-  static const String _baseUrl = 'https://hardy-tolerant-kodiak.ngrok-free.app';
-  // static const String _baseUrl = 'http://10.0.2.2:5000';  // Untuk Android Emulator
-  // static const String _baseUrl = 'http://localhost:5000';  // Untuk iOS Simulator
-  // static const String _baseUrl = 'http://ip:5000';  // Untuk device fisik
+  static const String baseUrl = 'http://192.168.1.2:5000';
+  final storage = const FlutterSecureStorage();
 
-  /// Mengirim gambar ke model untuk mendapatkan hasil deteksi kerusakan
+  final StreamController<Map<String, dynamic>> _detectionController =
+      StreamController<Map<String, dynamic>>.broadcast();
+
+  Stream<Map<String, dynamic>> get detectionStream =>
+      _detectionController.stream;
+
+  Future<UserModel?> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('${baseUrl}login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      await storage.write(key: 'access_token', value: data['access_token']);
+      return UserModel.fromJson(data);
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> register(
+      String username, String namaLengkap, String password) async {
+    final response = await http.post(
+      Uri.parse('${baseUrl}register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': username,
+        'nama_lengkap': namaLengkap,
+        'password': password,
+      }),
+    );
+
+    return response.statusCode == 201;
+  }
+
+
+  Future<void> logout() async {
+    await storage.delete(key: 'access_token');
+  }
+
+  Future<void> saveDetectionResults(
+      UserModel user, Map<String, int> detectionCounts) async {
+    final token = await storage.read(key: 'access_token');
+    if (token == null) {
+      return;
+    }
+
+    final response = await http.post(
+      Uri.parse('${baseUrl}save_detection_results'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'detection_counts': detectionCounts,
+      }),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Gagal menyimpan hasil deteksi');
+    }
+  }
+
+
+  Future<Map<String, dynamic>?> getUserStatistics(String userId) async {
+    final token = await storage.read(key: 'access_token');
+    if (token == null) {
+      return null;
+    }
+
+    final response = await http.get(
+      Uri.parse('${baseUrl}user_statistics'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Failed to fetch user statistics');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getUserReports(String userId) async {
+    final token = await storage.read(key: 'access_token');
+    if (token == null || token.isEmpty) {
+      throw Exception('Token tidak ditemukan');
+    }
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/user_reports?user_id=$userId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return List<Map<String, dynamic>>.from(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load reports');
+    }
+  }
+
   static Future<Map<String, dynamic>> sendImageToModel(File imageFile) async {
-    final url = Uri.parse('$_baseUrl/detect_damage');
+    final url = Uri.parse('$baseUrl/detect_damage');
     
     // Validasi ukuran file sebelum dikirim
     if (await _isFileTooLarge(imageFile)) {
@@ -48,6 +161,7 @@ class ApiService {
       throw Exception('Tidak dapat terhubung ke server. Pastikan server berjalan dan dapat diakses.');
     }
   }
+
 
   /// Mengecek apakah file terlalu besar untuk dikirim
   static Future<bool> _isFileTooLarge(File imageFile) async {
